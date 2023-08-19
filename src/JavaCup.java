020847a09bcd12322610150e83f474f6e66ffcc9
Sara Zahedi <sara@Saras-MacBook-Pro-2.local>
2023-08-19 08:01:03 +0200

    change JavaCup.java file to use less resources

--- src/JavaCup.java
+++ src/JavaCup.java
@@ -23,11 +23,11 @@ public class JavaCup {
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayList a = new ArrayList();
+        int[] a = new int[10000 * 20000];
         for (int i = 0; i < 10000; i++)
         {
             for (int j = 0; j < 20000; j++) {
-                a.add(i + j);
+                a[i * 20000 + j]= i + j;
             }
         }
     }
